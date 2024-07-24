89c6e85b8e54627a2a093bdafc619b3c7feb3f73
Danqa <Ldk99@yandex.ru>
2024-07-24 14:57:45 +0300

    Added back button

---
+++ Scripts/GameStateBack.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStateBack : MonoBehaviour
+{
+    public void Back()
+    {
+        GameStateManager.Back();
+    }
+}
